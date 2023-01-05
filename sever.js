0242da36962f520b47cc7a8b3ba4d72afdc30cee
Alinawaz-786 <55326140+Alinawaz-786@users.noreply.github.com>
2023-01-05 22:39:46 +0500

    first commit

--- sever.js
+++ sever.js
@@ -1,5 +1,15 @@
-const http = require('http');
+const http =  require('http');
 
-const server = http.createServer();
+const server =  http.createServer((req,res) =>{
+    // console.log(req.headers);
+	res.setHeader('Content-type','text/html');
+	res.write("<html>");
+	res.write("<head><title>Testing</title><head>");
+	res.write("<body><h1>This is the Testing Pages</h1></body>");
+	res.write("</html>");
+	res.end();
+
+	// process.exit();
+});
 
 server.listen(3000);
