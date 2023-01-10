1dfff99fb43fc5e6c9e6f8fb8fdf8e3276e3a367
Alinawaz-786 <55326140+Alinawaz-786@users.noreply.github.com>
2023-01-10 21:13:46 +0500

    Middleware

--- sever.js
+++ sever.js
@@ -1,6 +1,20 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes.handler);
+/*
+ * Middleware Add into the Routes
+ */
+
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("In to another middleware");
+    res.send('<h1>This is the another middleware Response');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
