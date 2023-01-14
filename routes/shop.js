fea73545b4780bc8a7fb174561b02fc045911c64
Alinawaz-786 <55326140+Alinawaz-786@users.noreply.github.com>
2023-01-14 20:28:57 +0500

    App use Middleware with use keywords

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
 const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
+const AdminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log("Shop file");
+    console.log(AdminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     // res.sendFile(path.join(rootDir, '../', 'views', 'shop.html'));
     // console.log("In to middleware");
