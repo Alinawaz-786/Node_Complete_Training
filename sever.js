1e779fef7c7041bcaf2219c3281b6563e82c879f
Alinawaz-786 <55326140+Alinawaz-786@users.noreply.github.com>
2023-01-15 23:42:17 +0500

    MVC File WRITE & READ

--- sever.js
+++ sever.js
@@ -3,13 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/errorController');
 
-
 const app = express();
 
 //set the view engine
 app.set("view engine", "ejs");
 // app.set("views", path.resolve(__dirname, "view/ejs"))
 
+//Controller SetUP
 const adminRoutes = require('./routes/admin');
 const ShopRouter = require('./routes/shop');
 
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes);
 app.use(ShopRouter);
-
+//Error Page Load
 app.use(errorController.get404);
 
 app.listen(3000);
