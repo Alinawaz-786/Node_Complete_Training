f9ad0bc3a6ab2f63d50106c93a1d9060bfe7fa22
Alinawaz-786 <alinawaz1857@gmail.com>
2024-01-02 15:19:09 +0500

    Push testCase

---
+++ test/start.js
@@ -0,0 +1,13 @@
+const expect = require('chai');
+
+// it('Should Add numbers correctly',function(){
+//     const num1 = 2;
+//     const num2 = 3;
+//     expect(num1+num2).to.equal(5);
+// })
+
+it('Should not give a result of 5',function(){
+    let num1 = 2;
+    let num2 = 3;
+    // expect(num1+num2).to.equal(5);
+})
